7dd443554dd37351105dc61739353ab0c86eef53
DmitryAkulich33 <Dmitry.Akulich.33@gmail.com>
2019-12-13 20:45:27 +0300

    added methods

--- task01/src/main/java/by/epam/exercise18/runner/Main.java
+++ task01/src/main/java/by/epam/exercise18/runner/Main.java
@@ -1,26 +1,48 @@
-package by.epam.exercise18.controller;
+package by.epam.exercise18.runner;
 
 import java.util.Scanner;
 
-public class Runner {
+public class Main {
     public static void main(String[] args) {
-        double cubeEdge;
-        double faceArea;
-        double cubeArea;
-        double cubeVolume;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of the cube edge:");
-        cubeEdge = scanner.nextDouble();
-        scanner.close();
-        faceArea = cubeEdge * cubeEdge;
-        cubeArea = faceArea * 6;
-        cubeVolume = faceArea * cubeEdge;
-        if (cubeEdge > 0) {
-            System.out.println("The face area of the cube is " + faceArea);
-            System.out.println("The area of the cube is " + cubeArea);
-            System.out.println("The volume of the cube is " + cubeVolume);
+        solve(3.0);
+        solve(0.0);
+    }
+
+    public static void solve (double cubeEdge){
+        if(cubeEdge > 0) {
+            double faceArea = findFaceArea(cubeEdge);
+            double cubeArea = findCubeArea(cubeEdge);
+            double cubeVolume = findCubeVolume(cubeEdge);
+            printCorrectData(faceArea, cubeArea, cubeVolume);
         } else {
-            System.out.println("The entered data is incorrect.");
+            printUnCorrectData();
         }
     }
+
+    public static double findFaceArea(double cubeEdge) {
+        return cubeEdge * cubeEdge;
+    }
+
+    public static double findCubeArea(double cubeEdge) {
+        return cubeEdge * cubeEdge * 6;
+    }
+
+    public static double findCubeVolume(double cubeEdge) {
+        return cubeEdge * cubeEdge * cubeEdge;
+    }
+
+    public static void printCorrectData(double faceArea, double cubeArea, double cubeVolume) {
+        System.out.println("The face area of the cube is " + faceArea);
+        System.out.println("The area of the cube is " + cubeArea);
+        System.out.println("The volume of the cube is " + cubeVolume);
+    }
+
+    public static void printUnCorrectData() {
+        System.out.println("The data is incorrect");
+    }
+
+
+
+
+
 }
