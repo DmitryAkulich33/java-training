830a5eacd956228914ef0e0651db0b870c029849
DmitryAkulich33 <Dmitry.Akulich.33@gmail.com>
2019-12-14 12:33:46 +0300

    added exception and main

--- task02/src/main/java/by/epam/nextdate/exception/WrongDateException.java
+++ task02/src/main/java/by/epam/nextdate/exception/WrongDateException.java
@@ -1,4 +1,20 @@
 package by.epam.nextdate.exception;
 
-public class WrongDateException {
+public class WrongDateException extends RuntimeException {
+    String message;
+
+    public WrongDateException() {
+    }
+
+    public WrongDateException(String message) {
+        super(message);
+    }
+
+    public WrongDateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongDateException(Throwable cause) {
+        super(cause);
+    }
 }
---
+++ task02/src/main/java/by/epam/nextdate/runner/Main.java
@@ -0,0 +1,7 @@
+package by.epam.nextdate.runner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
