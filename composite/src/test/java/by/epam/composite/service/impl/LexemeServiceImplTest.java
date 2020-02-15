ba55159148db3d546f5f9b539b09746a11f3e1d8
DmitryAkulich33 <Dmitry.Akulich.33@gmail.com>
2020-02-15 15:12:47 +0300

    added some test

---
+++ composite/src/test/java/by/epam/composite/service/impl/LexemeServiceImplTest.java
@@ -0,0 +1,13 @@
+package by.epam.composite.service.impl;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LexemeServiceImplTest {
+    private LexemeServiceImpl lexemeService = new LexemeServiceImpl();
+
+    @Test
+    public void testSort() {
+    }
+}
---
+++ composite/src/test/java/by/epam/composite/service/impl/SentenceServiceImplTest.java
@@ -0,0 +1,45 @@
+package by.epam.composite.service.impl;
+
+import by.epam.composite.domain.Component;
+import by.epam.composite.service.parser.ChainTextParser;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class SentenceServiceImplTest {
+    private SentenceServiceImpl sentenceService = new SentenceServiceImpl();
+
+    @DataProvider
+    public Object[][] forCorrectComponent(){
+        Component component1 = ChainTextParser.parse("    Hello. It is my the second text. It is the first paragraph.\\n    Then. It is the second paragraph.\\n    Bye.\\n");
+        Component component2  = ChainTextParser.parse("    Hello. It is my the text. second It is the first paragraph.\\n    Then. It is the second paragraph.\\n    Bye.\\n");
+        Component component3 = ChainTextParser.parse("    Hello. This is my text.\\n    Bye.\\n");
+        Component component4  = ChainTextParser.parse("    Hello. is my This text.\\n    Bye.\\n");
+        return new Object[][]{
+                {component1, component2},
+                {component3, component4},
+        };
+    }
+
+    @DataProvider
+    public Object[][] forWrongComponent(){
+        Component component1 = ChainTextParser.parse("    Hello. It is my the second text. It is the first paragraph.\\n    Then. It is the second paragraph.\\n    Bye.\\n");
+        Component component2  = ChainTextParser.parse("    Hello. It is my the text. second  the It is first paragraph.\\n    Then. It is the second paragraph.\\n    Bye.\\n");
+        Component component3 = ChainTextParser.parse("    Hello. This is my text.\\n    Bye.\\n");
+        Component component4  = ChainTextParser.parse("    Hello. This is my text.\\n    Bye.\\n");
+        return new Object[][]{
+                {component1, component2},
+                {component3, component4},
+        };
+    }
+
+    @Test(dataProvider = "forCorrectComponent")
+    public void testSortCorrectComponents(Component component1, Component component2) {
+        Assert.assertEquals(sentenceService.sort(component1).operation(), component2.operation());
+    }
+
+    @Test(dataProvider = "forWrongComponent")
+    public void testSortWrongComponents(Component component1, Component component2) {
+        Assert.assertNotEquals(sentenceService.sort(component1).operation(), component2.operation());
+    }
+}
